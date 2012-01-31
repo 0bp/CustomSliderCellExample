e7b78e1332fa8ecd0fd4340b1861dd89ce95384e
Boris <dev@borispenck.de>
2012-01-31 12:15:20 +0100

    No Comment

--- CustomSliderCellExample/main.m
+++ CustomSliderCellExample/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  CustomSliderCellExample
-//
-//  Created by Boris Penck on 31.01.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
